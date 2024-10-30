3a6583041794910585b80e705a2d1d6f14a57562
Zer0-Vector <77898583+Zer0-Vector@users.noreply.github.com>
2024-10-30 14:13:02 -0400

    Improve jekyll controller script

--- JekyllControl.ps1
+++ JekyllControl.ps1
@@ -1,15 +1,32 @@
-param( [switch]$Terminate )
+[CmdletBinding(DefaultParameterSetName="Status")]
+param(
+  [Parameter(ParameterSetName="Stop")]
+  [switch]$Terminate,
+  [PSDefaultValue()]
+  [Parameter(ParameterSetName="Status")]
+  [switch]$Status = ($true -and -not $Terminate)
+)
 
-$proc = Get-WmiObject Win32_Process -Filter "name = 'ruby.exe'" | where {$_.CommandLine -like '*jekyll serve*'}
-if ($proc -eq $null) {
+$wmiProc = Get-WmiObject Win32_Process -Filter "name = 'ruby.exe'" | where {$_.CommandLine -like '*jekyll serve*'}
+
+$proc = $null
+if ($wmiProc -eq $null) {
   Write-Host "No 'jekyll serve' process running"
-  exit
+  if ($Status) {
+    exit 1
+  } else {
+    exit
+  }
+} else {
+  $proc = Get-Process -PID ($wmiProc).ProcessId
 }
 
 if ($Terminate) {
-  $lc = $proc.CommandLine
+  $cl = $wmiProc.CommandLine
   Write-Host "Terminating '$cl'..."
-  $proc | Invoke-WmiMethod -Name Terminate
+  $wmiProc | Invoke-WmiMethod -Name Terminate | Out-Null
+  ($proc).WaitForExit()
+  Write-Host "done."
 } else {
-  $proc | Select-Object -Property ProcessId,CommandLine
+  $proc | Add-Member -MemberType AliasProperty -Name PID -Value Id -PassThru | Format-List ProcessName, PID, @{Label="Uptime"; Expression={(Get-Date) - ($_).StartTime}}
 }
